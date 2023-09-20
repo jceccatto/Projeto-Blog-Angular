afd5be7b4db730f7902f3d8166b84d5e17c8376e
Jh.Ceccatto <jbom6@hotmail.com>
2023-09-20 14:46:51 -0300

    Add Footer component and implemented

--- Blog/src/app/app.module.ts
+++ Blog/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ContentComponent } from './pages/content/content.component';
     BigCardComponent,
     SmallCardComponent,
     HomeComponent,
-    ContentComponent
+    ContentComponent,
+    FooterBarComponent
   ],
   imports: [
     BrowserModule,
